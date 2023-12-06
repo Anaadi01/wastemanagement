eeb6cdab6cd292ee6c0d0a4465e42f3ee19b4de8
Anaadi01 <harshitsuman820@gmail.com>
2023-12-06 17:50:56 +0530

    fdf

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ db.once('open', () => {
 });
 
 app.use(express.json());
-app.use(cors({ credentials: true, origin: 'http://localhost:5173' }));
+app.use(cors());
 // app.use(cors());
 app.use('/api', authenticationRoutes);
 app.use('/api', categoryRoutes);
